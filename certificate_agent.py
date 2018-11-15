4a73a54992212c7f9f98b51ee4cef1bb28ba4a17
Aaron Zhao <rudy710@qq.com>
2018-11-15 16:23:42 +0800

    month, day, year

--- certificate_agent.py
+++ certificate_agent.py
@@ -89,6 +89,7 @@ def main():
                     aws_key=args.aws_key,
                     long_course=course_name.encode('utf-8'),
                     issued_date=issued_date,
+                    json_date=json_date,
                     score=score,
                     pdf_info=pdf_info
                 )
