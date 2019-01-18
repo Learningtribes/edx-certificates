00d12bf61adf489fe870ed63a828af93660663bf
Aaron Zhao <rudy710@qq.com>
2019-01-18 10:30:51 +0800

    check the error

--- gen_cert.py
+++ gen_cert.py
@@ -261,6 +261,7 @@ class CertificateGen(object):
         template_pdf_filename = "{0}/certificate-template-edX-DemoX.pdf".format(template_prefix)
         print("test C")
         if template_pdf and pdf_info:
+            print("template_dir: ", pdf_info.get("template_dir"))
             template_pdf_filename = "{0}/{1}".format(pdf_info.get("template_dir"), template_pdf)
             if 'verified' in template_pdf:
                 self.template_type = 'verified'
@@ -271,7 +272,6 @@ class CertificateGen(object):
             log.critical("I/O error ({0}): {1} opening {2}".format(e.errno, e.strerror, template_pdf_filename))
             raise
 
-        print("template_dir: ", template_pdf_filename)
         self.cert_label_singular = cert_data.get('CERTS_ARE_CALLED', CERTS_ARE_CALLED)
         self.cert_label_plural = cert_data.get('CERTS_ARE_CALLED_PLURAL', CERTS_ARE_CALLED_PLURAL)
         self.course_association_text = cert_data.get('COURSE_ASSOCIATION_TEXT', 'a course of study')
