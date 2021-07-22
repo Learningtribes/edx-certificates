4bfc57d173599d3f3692ec4e54051696f9ddd86d
Aaron Zhao <rudy710@qq.com>
2021-07-22 18:26:58 +0800

    remove font

--- gen_cert.py
+++ gen_cert.py
@@ -457,12 +457,6 @@ class CertificateGen(object):
         addMapping('Klavika-Light', 1, 0, 'Klavika-Medium')
         addMapping('Klavika-Light', 1, 1, 'Klavika-Medium-Italic')
 
-        # add Georgia
-        addMapping('Georgia', 0, 0, 'Georgia')
-        addMapping('Georgia', 0, 1, 'Georgia')
-        addMapping('Georgia', 1, 0, 'Georgia')
-        addMapping('Georgia', 1, 1, 'Georgia')
-
         styleArial = ParagraphStyle(name="arial", leading=10, fontName='Arial Unicode')
         styleOpenSans = ParagraphStyle(name="opensans-regular", leading=10, fontName='OpenSans-Regular')
         styleOpenSansLight = ParagraphStyle(name="opensans-light", leading=10, fontName='OpenSans-Light')
