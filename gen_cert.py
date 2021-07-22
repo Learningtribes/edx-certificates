c049e26dadafba2778a7cca1631e1c3b0fde565e
Aaron Zhao <rudy710@qq.com>
2021-07-22 16:20:20 +0800

    add new font

--- gen_cert.py
+++ gen_cert.py
@@ -457,15 +457,15 @@ class CertificateGen(object):
         addMapping('Klavika-Light', 1, 0, 'Klavika-Medium')
         addMapping('Klavika-Light', 1, 1, 'Klavika-Medium-Italic')
 
-        addMapping('DroidSerif', 0, 0, 'DroidSerif-Regular')
-        addMapping('DroidSerif', 0, 1, 'DroidSerif-Italic')
-        addMapping('DroidSerif', 1, 0, 'DroidSerif-Bold')
-        addMapping('DroidSerif', 1, 1, 'DroidSerif-BoldItalic')
+        # add San serif
+        addMapping('SansSerif', 0, 0, 'SansSerif')
+        addMapping('SansSerif', 0, 1, 'SansSerif')
+        addMapping('SansSerif', 1, 0, 'SansSerif')
+        addMapping('SansSerif', 1, 1, 'SansSerif')
 
         styleArial = ParagraphStyle(name="arial", leading=10, fontName='Arial Unicode')
         styleOpenSans = ParagraphStyle(name="opensans-regular", leading=10, fontName='OpenSans-Regular')
         styleOpenSansLight = ParagraphStyle(name="opensans-light", leading=10, fontName='OpenSans-Light')
-        styleSerif = ParagraphStyle(name="droidserif", leading=10, fontName='DroidSerif')
 
         if not self.pdf_info:
             # if there is no pdf template config in django admin,
@@ -678,8 +678,6 @@ class CertificateGen(object):
             font = self.pdf_info.get('font')
             if font == 'Arial':
                 style = styleArial
-            elif font == 'DroidSerif':
-                style = styleSerif
             else:
                 style = ParagraphStyle(name=font.lower(), leading=10, fontName=font)
             style.alignment = TA_CENTER
