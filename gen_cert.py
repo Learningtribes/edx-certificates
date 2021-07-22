28a84cdad72086d5f5748ab4236186db22d48049
Aaron Zhao <rudy710@qq.com>
2021-07-22 17:09:24 +0800

    add Georgia

--- gen_cert.py
+++ gen_cert.py
@@ -457,11 +457,11 @@ class CertificateGen(object):
         addMapping('Klavika-Light', 1, 0, 'Klavika-Medium')
         addMapping('Klavika-Light', 1, 1, 'Klavika-Medium-Italic')
 
-        # add San serif
-        addMapping('SansSerif', 0, 0, 'SansSerif')
-        addMapping('SansSerif', 0, 1, 'SansSerif')
-        addMapping('SansSerif', 1, 0, 'SansSerif')
-        addMapping('SansSerif', 1, 1, 'SansSerif')
+        # add Georgia
+        addMapping('Georgia', 0, 0, 'Georgia-Regular')
+        addMapping('Georgia', 0, 1, 'Georgia-Italic')
+        addMapping('Georgia', 1, 0, 'Georgia-Bold')
+        addMapping('Georgia', 1, 1, 'Georgia-BoldItalic')
 
         styleArial = ParagraphStyle(name="arial", leading=10, fontName='Arial Unicode')
         styleOpenSans = ParagraphStyle(name="opensans-regular", leading=10, fontName='OpenSans-Regular')
