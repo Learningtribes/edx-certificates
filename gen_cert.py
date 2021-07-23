8ebd6514855234007145b77e34df92b01410ef0f
Aaron Zhao <rudy710@qq.com>
2021-07-23 10:10:29 +0800

    add new font

--- gen_cert.py
+++ gen_cert.py
@@ -457,6 +457,12 @@ class CertificateGen(object):
         addMapping('Klavika-Light', 1, 0, 'Klavika-Medium')
         addMapping('Klavika-Light', 1, 1, 'Klavika-Medium-Italic')
 
+        # add Georgia
+        addMapping('Georgia', 0, 0, 'Georgia')
+        addMapping('Georgia', 0, 1, 'Georgia-Italic')
+        addMapping('Georgia', 1, 0, 'Georgia-Bold')
+        addMapping('Georgia', 1, 1, 'Georgia-BoldItalic')
+
         styleArial = ParagraphStyle(name="arial", leading=10, fontName='Arial Unicode')
         styleOpenSans = ParagraphStyle(name="opensans-regular", leading=10, fontName='OpenSans-Regular')
         styleOpenSansLight = ParagraphStyle(name="opensans-light", leading=10, fontName='OpenSans-Light')
