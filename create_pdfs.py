973d94132d568bdc78ceff850c62f93e2cf2b3a9
rythmE <poiuwertion@gmail.com>
2021-11-29 11:18:55 +0800

    Debug {completion_date}

--- create_pdfs.py
+++ create_pdfs.py
@@ -115,9 +115,7 @@ def main():
                 dir_prefix=pdf_dir,
                 long_org=args.long_org,
                 long_course=args.long_course,
-                issued_date=args.issued_date,
-                duration=args.duration,
-                completion_date=args.completion_date,
+                issued_date=args.issued_date
             )
             title = None
             if args.assign_title:
