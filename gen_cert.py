bbe2112c128a2c8f613519c5c59a677b4f368d8b
Cristiano de Paula <crdepa@gmail.com>
2024-03-25 14:39:22 -0300

    fixing function to increase png quality

--- gen_cert.py
+++ gen_cert.py
@@ -825,21 +825,18 @@ class CertificateGen(object):
             pdf_document = fitz.open(pdf_path)
             first_page = pdf_document[0]
 
-            # zoom_x = 2.0  # horizontal zoom
-            # zoom_y = 2.0  # vertical zoom
-            # mat = fitz.Matrix(zoom_x, zoom_y)  # zoom factor 2 in each dimension
-            # pix = first_page.get_pixmap(matrix=mat)
-            pix = first_page.getPixmap(alpha=False)
-
-#            ratio = output_resolution / 72.0
+            zoom_x = 3.0  # horizontal zoom
+            zoom_y = 3.0  # vertical zoom
+            mat = fitz.Matrix(zoom_x, zoom_y)
+            pix = first_page.getPixmap(alpha=False, matrix=mat)
+            ratio = output_resolution / 72.0
 
             # resizing image
-#            width = int(pix.width * ratio)
-#            height = int(pix.height * ratio)
-#            image = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
-#            resized_image = image.resize((width, height), Image.BICUBIC) # ANTIALIAS, BICUBIC, LANCZOS
-#            resized_image.save(filename_png, dpi=(output_resolution, output_resolution))
-            pix.save(filename_png)
+            width = int(pix.width * ratio)
+            height = int(pix.height * ratio)
+            image = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+            resized_image = image.resize((width, height), Image.ANTIALIAS)
+            resized_image.save(filename_png, dpi=(output_resolution, output_resolution))
             return True
         except Exception as e:
             log.info("DEBUG: Convert Error: %s" % e)
