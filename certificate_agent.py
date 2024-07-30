4edd9318b6ad8e5e5943ffabb9fc0084b522b530
tassadit <tassadite.sadoun@gmail>
2024-07-30 20:41:54 +0200

    Revert "png_url instead of download_url_png"
    This reverts commit 1d1652c3684a903b474f353b85251154a0cca487.

--- certificate_agent.py
+++ certificate_agent.py
@@ -181,7 +181,7 @@ def main():
                 (download_uuid,
                  verify_uuid,
                  download_url,
-                 png_url) = cert.create_and_upload(name.encode('utf-8'),
+                 download_url_png) = cert.create_and_upload(name.encode('utf-8'),
                                                         username.encode('utf-8'),
                                                         employee_id,
                                                         grade=grade,
@@ -250,7 +250,7 @@ def main():
                     'username': username,
                     'course_id': course_id,
                     'url': download_url,
-                    'png_url': png_url,
+                    'url_png': download_url_png,
                 }),
             }
             log.info("Posting result to the LMS: {0}".format(xqueue_reply))
